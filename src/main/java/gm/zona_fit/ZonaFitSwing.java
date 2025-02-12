df6111554fc945e16e3ca4f5c24ff8e55f1346ce
Robinson Lopez <robins.616009@gmail.com>
2025-02-11 19:03:24 -0500

    Modificaciones librerias JSF - Primefaces y Spring

--- src/main/java/gm/zona_fit/ZonaFitSwing.java
+++ src/main/java/gm/zona_fit/ZonaFitSwing.java
@@ -8,7 +8,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import javax.swing.*;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ZonaFitSwing {
     public static void main(String[] args) {
         // configuramos el modo oscuro
